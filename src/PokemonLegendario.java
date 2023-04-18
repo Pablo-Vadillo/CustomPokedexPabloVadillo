f1fba68e059caae8e3842afe846970b22281dc25
NotVadillo <usr3588@salesianos-lacuesta.net>
2023-04-18 09:47:59 +0100

    feat: pokemonlegendario methods created

--- src/PokemonLegendario.java
+++ src/PokemonLegendario.java
@@ -1,2 +1,24 @@
-public class PokemonLegendario {
+public class PokemonLegendario extends Pokemon{
+    // Atributos
+    private String localizacion;
+
+    // Constructores
+    public PokemonLegendario(String nombre, String tipo, double altura, double peso, String descripcion, String localizacion) {
+        super(nombre, tipo, altura, peso, descripcion);
+        this.localizacion = localizacion;
+    }
+
+    public PokemonLegendario(String nombre, String tipo, String localizacion) {
+        super(nombre, tipo);
+        this.localizacion = localizacion;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        String info = super.toString();
+        info += "Localización: " + localizacion + "\n";
+        return info;
+    }
 }
+
